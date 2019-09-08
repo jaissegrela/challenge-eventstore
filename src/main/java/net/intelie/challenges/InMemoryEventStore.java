f4da7b46fe7f0605cf39380c89eb1acf3a8b3d7f
Jaisse Grela <jgrela@gmail.com>
2019-09-08 16:40:36 -0300

    InMemoryEventStore and TimeEventIterator were implemented

--- src/main/java/net/intelie/challenges/InMemoryEventStore.java
+++ src/main/java/net/intelie/challenges/InMemoryEventStore.java
@@ -1,23 +1,46 @@
 package net.intelie.challenges;
 
+import java.util.Hashtable;
+import java.util.Vector;
+
+
+/**
+ * An event store.
+ * <p>
+ * InMemoryEventStore is an in-memory implementation of event store
+ */
 public class InMemoryEventStore implements EventStore {
 
-	@Override
-	public void insert(Event event) {
-		// TODO Auto-generated method stub
+	protected Hashtable<String, Vector<Event>> store;
+	
+	public InMemoryEventStore()
+	{
+		store = new Hashtable<String, Vector<Event>>();
+	}
 
+	@Override
+	public synchronized void insert(Event event) {
+		if (event != null) {
+			Vector<Event> temp = store.get(event.type());
+			if (temp == null) {
+				temp = new Vector<Event>();
+				store.put(event.type(), temp);
+			}
+			temp.add(event);
+		}
 	}
 
 	@Override
 	public void removeAll(String type) {
-		// TODO Auto-generated method stub
-
+		store.remove(type);
 	}
 
 	@Override
 	public EventIterator query(String type, long startTime, long endTime) {
-		// TODO Auto-generated method stub
-		return null;
+		Vector<Event> temp = store.get(type);
+		if(temp == null)
+			temp = new Vector<Event>();
+		return new TimeEventIterator(temp, startTime, endTime);
 	}
 
 }
--- src/main/java/net/intelie/challenges/TimeEventIterator.java
+++ src/main/java/net/intelie/challenges/TimeEventIterator.java
@@ -2,35 +2,53 @@ package net.intelie.challenges;
 
 import java.util.Vector;
 
+/**
+ * An iterator over an event collection.
+ * <p>
+ * This iterator filters an event collection by timestamp range disregarding
+ * type
+ */
 public class TimeEventIterator implements EventIterator {
 
-	
-	public TimeEventIterator(Vector<Event> events, int startTime, int endTime) {
-		// TODO Auto-generated constructor stub
+	protected Vector<Event> events;
+	protected int index;
+	protected Event current;
+	protected long startTime, endTime;
+
+	protected TimeEventIterator(Vector<Event> events, long startTime, long endTime) {
+		this.events = events;
+		this.startTime = startTime;
+		this.endTime = endTime;
+		index = -1;
 	}
 
 	@Override
 	public void close() throws Exception {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
-	public boolean moveNext() {
-		// TODO Auto-generated method stub
+	public synchronized boolean moveNext() {
+		while (++index < events.size()) {
+			current = events.get(index);
+			if (startTime <= current.timestamp() && current.timestamp() < endTime)
+				return true;
+		}
+		current = null;
 		return false;
 	}
 
 	@Override
 	public Event current() {
-		// TODO Auto-generated method stub
-		return null;
+		if (current == null)
+			throw new IllegalStateException();
+		return current;
 	}
 
 	@Override
-	public void remove() {
-		// TODO Auto-generated method stub
-
+	public synchronized void remove() {
+		if (current == null)
+			throw new IllegalStateException();
+		events.remove(index--);
 	}
 
 }
